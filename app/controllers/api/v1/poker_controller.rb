d5b0644d9fddeaf0cc6ae6d88a697cdf673b4d10
hero <hero@asterisk-works.jp>
2016-04-03 22:52:32 +0900

    Add comment

--- app/controllers/api/v1/poker_controller.rb
+++ app/controllers/api/v1/poker_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::PokerController < ApplicationController
   protect_from_forgery with: :null_session
 
+  # 役の判定を行うAPI
+  #
+  # このアクションでは、複数の手札間の関係に関する処理として次を行っている
+  # - 最も強い役の判定
+  # - 手札間のカードの重複の検出
+  # 本来、ディーラーを担うクラスを設け、そのクラスの責務とすべきかもしれないが、
+  # 今回の仕様では役割が少ないのでアクションの中で実装した。
+  #
+  # params cards [Array] 手札の文字列表現のリスト
   def judge
     params_cards = params[:cards]
     raise 'no card' if params_cards.blank?
